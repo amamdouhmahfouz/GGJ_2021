ef409fe7a81351b74d329498bc912e69d736e4c3
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-01-29 00:49:21 +0200

    writing commands on screen

--- GGJ_2021/Main.cs
+++ GGJ_2021/Main.cs
@@ -73,8 +73,12 @@ namespace GGJ_2021
             SceneManager.Start();
 
             SceneManager.AddInitializer(MainScene, 0);
+            SceneManager.AddInitializer(MainMenu, 1);
+            SceneManager.AddInitializer(PlayScene, 2);
             //////////////////////////////////////////////////////////
-            SceneManager.LoadScene(new Scene("MainScene", 0)); //Main Scene
+            //SceneManager.LoadScene(new Scene("MainScene", 0)); //Main Scene
+            //SceneManager.LoadScene(new Scene("MainMenu", 1));
+            SceneManager.LoadScene(new Scene("PlayScene", 2));
         }
 
         private void MainScene()
@@ -85,6 +89,101 @@ namespace GGJ_2021
             SceneManager.ActiveScene.Start();
 
             //Initialization here
+        }
+
+        private void PlayScene()
+        {
+            // TODO: use this.Content to load your game content here
+            spriteFont = Content.Load<SpriteFont>("Font");
+
+            GameObject Monitor = new GameObject();
+            Monitor.AddComponent<Transform>(new Transform());
+            Monitor.AddComponent<SpriteRenderer>(new SpriteRenderer());
+            Monitor.GetComponent<SpriteRenderer>().Sprite = new Sprite(Monitor.Transform);
+            Monitor.GetComponent<SpriteRenderer>().Sprite.LoadTexture("Screen");
+            //Monitor.GetComponent<SpriteRenderer>().Sprite.DynamicScaledRect();
+
+
+            GameObject CommandTxt = new GameObject();
+            CommandTxt.AddComponent<Transform>(new Transform());
+            CommandTxt.AddComponent<WritableCommand>(new WritableCommand(spriteFont));
+            //PlayTxt.AddComponent<SpriteRenderer>(new SpriteRenderer());
+            CommandTxt.Tag = "CommandTxt";
+
+
+            SceneManager.ActiveScene.AddGameObject(Monitor);
+            SceneManager.ActiveScene.AddGameObject(CommandTxt);
+            SceneManager.ActiveScene.Start();
+
+            //Initialization here
+            CommandTxt.GetComponent<WritableCommand>().Color = Color.DarkSeaGreen;
+            CommandTxt.GetComponent<WritableCommand>().CustomOrigin = true;
+            //CommandTxt.GetComponent<WritableCommand>().Origin = new Vector2(200, 100);
+            CommandTxt.Transform.Position = new Vector2(900, 100);
+            CommandTxt.Transform.Scale *= 0.5f;
+            Monitor.Transform.Scale = new Vector2(0.75f, 0.75f);
+            Monitor.Transform.Position = new Vector2(150,100);
+        }
+
+
+        private void MainMenu()
+        {
+
+            spriteFont = Content.Load<SpriteFont>("Font");
+            // TODO: use this.Content to load your game content here
+            GameObject Canvas = new GameObject();
+            Canvas.AddComponent<Transform>(new Transform());
+            Canvas.AddComponent<Canvas>(new Canvas(Camera));
+            Canvas.Tag = "Canvas";
+
+            GameObject MenuPanel = new GameObject();
+            MenuPanel.AddComponent<Transform>(new Transform());
+            MenuPanel.AddComponent<Panel>(new Panel("Main Menu"));
+            MenuPanel.Tag = "Menu";
+            
+            
+            Canvas.AddChild(MenuPanel);
+
+            GameObject PlayBtn = new GameObject();
+            PlayBtn.AddComponent<Transform>(new Transform());
+            PlayBtn.AddComponent<Button>(new Button());
+
+           
+
+            GameObject PlayTxt = new GameObject();
+            PlayTxt.AddComponent<Transform>(new Transform());
+            PlayTxt.AddComponent<Text>(new Text("PlayText", spriteFont));
+            //PlayTxt.AddComponent<SpriteRenderer>(new SpriteRenderer());
+            PlayTxt.Tag = "PlayTxt";
+
+            //MenuPanel.AddChild(PlayBtn);
+            PlayBtn.AddChild(PlayTxt);
+            
+            
+
+
+            SceneManager.ActiveScene.AddGameObject(Canvas);
+            SceneManager.ActiveScene.AddGameObject(MenuPanel);
+            SceneManager.ActiveScene.AddGameObject(PlayBtn);
+            SceneManager.ActiveScene.AddGameObject(PlayTxt);
+
+
+            SceneManager.ActiveScene.Start();
+
+            //Initialization here
+            MenuPanel.GetComponent<Panel>().Color = Color.White;
+            MenuPanel.GetComponent<Panel>().Texture = Content.Load<Texture2D>("2k_earth_daymap");
+            MenuPanel.GetComponent<Panel>().Origin = MenuPanel.GetComponent<Panel>().Texture.Bounds.Size.ToVector2() / 2;
+            MenuPanel.GetComponent<Panel>().FillTheScreen();
+            PlayBtn.GetComponent<Button>().IdleColor = Color.White;
+            PlayBtn.GetComponent<Button>().HighlightColor = Color.DarkRed;
+            PlayBtn.Transform.Position = new Vector2(graphics.PreferredBackBufferWidth/2, graphics.PreferredBackBufferHeight/2);
+            PlayTxt.GetComponent<Text>().text = "Hello";
+            PlayTxt.GetComponent<Text>().Color = Color.Green;
+            PlayTxt.GetComponent<Transform>().LocalScale = new Vector2(0.5f);
+
+            //MenuPanel.Layer = 0;
+
         }
 
         /// <summary>
@@ -117,7 +216,17 @@ namespace GGJ_2021
 
             //passing a property as a refrence using delegates
             //Arrow.GetComponent<PropertiesAnimator>().GetKeyFrame("Rotate360").GetFeedback(value => Arrow.Transform.Rotation = value);
-            
+
+
+            //var keyboardState = Keyboard.GetState();
+            //var keys = keyboardState.GetPressedKeys();
+            //if (keys.Length > 0)
+            //{
+            //    var keyValue = keys[0].ToString();
+            //    SceneManager.ActiveScene.FindGameObjectWithTag("PlayTxt").GetComponent<Text>().text += keyValue.ToString();
+            //}
+
+
             SceneManager.ActiveScene.Update(gameTime);
 
             base.Update(gameTime);
@@ -134,7 +243,7 @@ namespace GGJ_2021
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone, null, Camera.GetViewTransformationMatrix()); // -> Mandatory
 
             SceneManager.ActiveScene.Draw(spriteBatch);
-
+            //spriteBatch.DrawString(spriteFont, "Play", new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2), Color.Red);
             //spriteBatch.DrawString(spriteFont, ((int)(1/this.TargetElapsedTime.TotalSeconds)).ToString(), Vector2.Zero, Color.Red); =>FPS
 
             spriteBatch.End();
@@ -142,3 +251,19 @@ namespace GGJ_2021
         }
     }
 }
+
+
+/*
+ * class WritableCommand : public GameObjectComponent {
+ * 
+ *  public string command;
+ *  
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * }
+ */
---
+++ GGJ_2021/Scripts/WritableCommand.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MyEngine;
+
+namespace GGJ_2021
+{
+    public class WritableCommand : GameObjectComponent
+    {
+
+        public string textCommand;
+        public SpriteFont Font;
+        public Vector2 Origin;
+        public Color Color;
+        public bool CustomOrigin = false;
+        public SpriteEffects spriteEffects;
+        public string[] splitCommands;
+
+        private Transform transform;
+        private float prevTime;
+        private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZD0D1D2D3D4D5D6D7D8D9";
+
+
+        public WritableCommand(SpriteFont font)
+        {
+            textCommand = "";
+            if (gameObject != null)
+                transform = gameObject.Transform;
+            Origin = Vector2.Zero;
+            Color = Color.Green;
+            Font = font;
+            spriteEffects = SpriteEffects.None;
+            prevTime = 0.0f;
+        }
+
+
+        public override void Start()
+        {
+            gameObject.Layer = LayerUI.GetLayer("Text");
+            if (transform == null)
+                transform = gameObject.Transform;
+
+            //transform.Position = new Vector2(Setup.graphics.PreferredBackBufferWidth / 2, Setup.graphics.PreferredBackBufferHeight / 2);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!CustomOrigin)
+                Origin = Font.MeasureString(textCommand) * 0.5f * transform.Scale;
+
+            spriteBatch.DrawString(Font, textCommand, transform.Position, Color, MathHelper.ToRadians(transform.Rotation), Origin, transform.Scale, spriteEffects, gameObject.Layer);
+        }
+
+        public void LoadFont(string Name)
+        {
+            Font = Setup.Content.Load<SpriteFont>(Name);
+        }
+
+        
+ 
+        public override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+            var keys = keyboardState.GetPressedKeys();
+            float nowTime = (float)gameTime.TotalGameTime.TotalSeconds;
+
+            if (keys.Length > 0 && (nowTime-prevTime) >= 0.17)
+            {
+                prevTime = nowTime;
+                var keyValue = keys[0].ToString();
+                System.Console.WriteLine(keyValue);
+                if (keyValue == "Enter") // new line, i.e new command
+                {
+                    // Split the whole textCommand split into separate lines
+                    string[] stringSeparators = new string[] { "\n" };
+                    splitCommands = textCommand.Split(stringSeparators, StringSplitOptions.None);
+
+                    // Add newline
+                    textCommand += "\n";
+                    System.Console.WriteLine(textCommand);
+
+                    // Check if command written by user is an actual command
+                    bool a = Array.Exists(splitCommands, element => element == "DRAWRECTANGLE");
+
+                }
+                else if (keyValue == "Back") //delete a character
+                {
+                    if (textCommand.Length != 0 && textCommand[textCommand.Length - 1] != '\n')
+                        textCommand = textCommand.Remove(textCommand.Length-1);
+
+                    string[] stringSeparators = new string[] { "\n" };
+                    splitCommands = textCommand.Split(stringSeparators, StringSplitOptions.None);
+                }
+
+                else if (alphabet.Contains(keyValue))
+                {
+                    if (keyValue.Length == 2)
+                        textCommand += keyValue[1].ToString();
+                    else
+                        textCommand += keyValue.ToString();
+                }
+                
+            }
+        }
+
+        public override GameObjectComponent DeepCopy(GameObject Clone)
+        {
+            WritableCommand clone = this.MemberwiseClone() as WritableCommand;
+            clone.transform = Clone.Transform;
+            Clone.Layer = LayerUI.GetLayer("WritableCommand");
+
+            return clone;
+        }
+
+
+    }
+}
